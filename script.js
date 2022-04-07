0a262eaeec9b445d9361035a376045d6375e49c8
reidisbot <101140697+reidisbot@users.noreply.github.com>
2022-04-06 20:50:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function getVal() {
+    const val = document.querySelector('input').value;
+    console.log(val);
+}
+function getItem(item) {
+    fetch('https://api.hypixel.net/skyblock/bazaar')
+}
